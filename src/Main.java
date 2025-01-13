b63f37b21d72b056cf7836e5d835f6036d34ec5c
Dima <barth0l0mew@mail.ru>
2025-01-13 18:05:16 +0300

    New Branch Mirenkov

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("New GitTest");
         System.out.println("Hello world!");
+        printNewBranch();
+    }
+    public static void printNewBranch (){
+        System.out.println("New branch");
     }
 }
